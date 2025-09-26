1af5a495bf8b46866a803d83d72474803c44b93d
uku <hi@uku.moe>
2025-09-26 23:49:18 +0200

    feat: 1.21.9

--- src/main/java/com/github/getchoo/smoothboot/mixin/client/IntegratedServerMixin.java
+++ src/main/java/com/github/getchoo/smoothboot/mixin/client/IntegratedServerMixin.java
@@ -4,9 +4,9 @@ import com.github.getchoo.smoothboot.SmoothBoot;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.server.SaveLoader;
-import net.minecraft.server.WorldGenerationProgressListenerFactory;
 import net.minecraft.server.integrated.IntegratedServer;
 import net.minecraft.util.ApiServices;
+import net.minecraft.world.chunk.ChunkLoadProgress;
 import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(IntegratedServer.class)
 public class IntegratedServerMixin {
     @Inject(method = "<init>", at = @At("RETURN"))
-    public void onInit(Thread serverThread, MinecraftClient client, LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, ApiServices apiServices, WorldGenerationProgressListenerFactory worldGenerationProgressListenerFactory, CallbackInfo ci) {
+    public void onInit(Thread serverThread, MinecraftClient client, LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, ApiServices apiServices, ChunkLoadProgress chunkLoadProgress, CallbackInfo ci) {
         serverThread.setPriority(SmoothBoot.config.threadPriority.integratedServer);
         SmoothBoot.LOGGER.debug("Initialized integrated server thread");
     }
